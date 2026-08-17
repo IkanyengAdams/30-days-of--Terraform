bff0def25a2feffffbaccbf07aab6ae3d4c52657
Ikanyeng Adams <ikanyengadams2@gmail.com>
2026-08-17 02:05:38 +0200

    practiced type constraints

---
+++ day_07/main.tf
@@ -0,0 +1,38 @@
+
+resource "aws_instance" "example" {
+  count = var.instance_count
+  ami = "ami-0bdc7d025135d7b49" # Amazon Linux 2 AMI
+  instance_type = var.instance_vm_type[0]
+  # region = tolist(var.allowed_region)[2]
+  region = var.config.region
+
+  monitoring = var.monitoring_enabled
+  associate_public_ip_address = var.associate_public_ip_address
+
+  tags = var.tags
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+ 
+
+  tags = var.tags
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = var.cidr_block[0]
+  from_port         = var.ingress_values[0]
+  ip_protocol       = var.ingress_values[1]
+  to_port           = var.ingress_values[2]
+}
+
+
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
---
+++ day_07/variables.tf
@@ -0,0 +1,75 @@
+variable "environment" {
+  default = "dev"
+  type = string
+}
+
+
+variable "region" {
+  type = string
+  default = "us-east-1"
+}
+
+variable "instance_count" {
+  description = "Number of EC2 instances to create"
+  type = number
+
+}
+
+variable "monitoring_enabled" {
+  description = "Enable detailed monitoring for EC2 instances"
+  type = bool
+  default = true
+}
+
+variable "associate_public_ip_address" {
+  description = "Associate public IP address with EC2 instances"
+  type = bool
+  default = true
+}
+
+variable "cidr_block" {
+  description = "CIDR block for the VPC"
+  type = list(string)
+  default = ["10.0.0.0/8", "192.168.0.0/16", "172.16.0.0/12"]
+}
+
+variable "instance_vm_type" {
+  description = "what type of instance to use"
+  type = list(string)
+  default = [ "t2.micro", "t3.micro", "t2.medium" ]
+}
+
+variable "allowed_region" {
+  description = "List of allowed AWS regions"
+  type = set(string)
+  default = [ "us-west-2", "eu-west-1", "us-east-1" ]
+}
+
+variable "tags" {
+  type = map(string)
+  default = {
+   Environment = "dev"
+    Name = "dev-Instance"
+    created_by = "terraform"
+
+  }
+  }
+
+variable "ingress_values" {
+  type = tuple([ number, string, number ])
+  default = [ 443, "tcp", 443 ]
+}
+
+variable "config" {
+  type = object({
+    region = string,
+    monitoring = bool,
+    instance_count = number
+
+  })
+  default = {
+    region = "us-east-1",
+    monitoring = true,
+    instance_count = 1
+  }
+}
